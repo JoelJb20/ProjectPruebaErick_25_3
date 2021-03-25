b6bc2826b3e51f5bb8f2c9699676ba58669c38f0
joel_ <joel_@DESKTOP-EAUO6T0>
2021-03-25 14:02:05 -0400

    WE PUT A MAXIMIZE WINDOWS AND DALYE 2 SEGUND AFTER MAXIMIZE..

--- Scripts/TEST_CASE_USUARIO_NP/Script1616687809842.groovy
+++ Scripts/TEST_CASE_USUARIO_NP/Script1616687809842.groovy
@@ -21,6 +21,10 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.amazon.com/-/es/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2FSwitch-GANA-Splitter-Pigtail-Supports%2Fdp%2FB06VX1PKQ7%2Fref%3Dnav_custrec_signin%3F_encoding%3DUTF8%26pd_rd_i%3DB06VX1PKQ7%26pd_rd_r%3D678BMQYFVGA6J80WRXM9%26pd_rd_w%3D2B37N%26pd_rd_wg%3DpyAfN%26psc%3D1%26refRID%3D678BMQYFVGA6J80WRXM9&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
 
+WebUI.maximizeWindow()
+
+WebUI.delay(2)
+
 WebUI.verifyElementPresent(findTestObject('AmazonLogin/Page_Amazon Iniciar sesin/input_Direccin de correo electrnico o nmero de telfono mvil_email'), 
     0)
 
